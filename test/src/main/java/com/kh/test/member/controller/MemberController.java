4d829617db48896faa4f21e69e5d7a61746c3341
박세진 <psjj4355@naver.com>
2025-12-09 12:53:35 +0900

    추가

---
+++ test/src/main/java/com/kh/test/member/controller/MemberController.java
@@ -0,0 +1,10 @@
+package com.kh.test.member.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins="http://lcoalhost:5173")
+@RestController
+public class MemberController {
+
+}
